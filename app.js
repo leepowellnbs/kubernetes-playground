699ce44fbc8c48fa2a664f8ea23355d9988dbaa6
Lee Powell <lee@ems.ai>
2023-04-24 11:48:49 +0100

    Commit

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 8888;
 
 app.get('/', (req, res) => {
-  res.send(`Hello World! ${process.env.MY_POD_IP}`);
+  res.send(`
+    <div>NODE NAME: ${process.env.MY_NODE_NAME}</div>
+    <div>POD NAME: ${process.env.MY_POD_NAME}</div>
+    <div>POD NAMESPACE: ${process.env.MY_POD_NAMESPACE}</div>
+    <div>POD IP: ${process.env.MY_POD_IP}</div>
+    <div>POD SERVICE ACCOUNT: ${process.env.MY_POD_SERVICE_ACCOUNT}</div>
+  `);
 });
 
 app.listen(port, () => {
